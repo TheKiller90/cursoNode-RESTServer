ef42a7a438f8181f58dce59a075e098679e1a54d
HazelP <antonio.pimentel.hazel@gmail.com>
2023-03-31 23:28:51 -0600

    Separación de rutas y controlador de la clase

--- models/server.js
+++ models/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 
+const router = require('../routes/usuarios');
+
 class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
 
         // Middlewares
         this.middlewares();
@@ -16,41 +19,13 @@ class Server {
     middlewares() {
         // CORS
         this.app.use(cors());
-        
+
         // Directorio público
         this.app.use(express.static('public'));
     }
 
     routes() {
-        this.app.get('/api', (req, res) => {
-            res.json({
-                msg: 'get API'
-            });
-        });
-
-        this.app.put('/api', (req, res) => {
-            res.status(500).json({
-                msg: 'put API'
-            });
-        });
-
-        this.app.post('/api', (req, res) => {
-            res.status(201).json({
-                msg: 'post API'
-            });
-        });
-
-        this.app.delete('/api', (req, res) => {
-            res.json({
-                msg: 'delete API'
-            });
-        });
-
-        this.app.patch('/api', (req, res) => {
-            res.json({
-                msg: 'patch API'
-            });
-        });
+        this.app.use(this.usuariosPath, router);
     }
 
     listen() {
